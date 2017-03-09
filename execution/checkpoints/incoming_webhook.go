3f637af5cdf322f0ae77beefdfed5f5fccf88243
Francois Hill <fhill@antennamagus.com>
2017-03-09 08:25:34 +0200

    feat: logging improvement for incoming_webhook and also reply with FORM and specify TOKEN in GET request.

--- execution/checkpoints/incoming_webhook.go
+++ execution/checkpoints/incoming_webhook.go
@@ -23,10 +23,10 @@ type incomingWebhook struct {
 	listenHTTPAddress string
 	randomTokenLength int
 
-	resultChan              chan<- *incomingWebhookResult
-	randomToken             string
-	abortKeyword            string
-	handlePostRequestLogger logging.Logger
+	resultChan           chan<- *incomingWebhookResult
+	randomToken          string
+	abortKeyword         string
+	handleRequestsLogger logging.Logger
 }
 
 type incomingWebhookResult struct {
@@ -34,36 +34,71 @@ type incomingWebhookResult struct {
 	ServerClosed bool
 }
 
-func (i *incomingWebhook) handlePostRequest(w http.ResponseWriter, r *http.Request) {
-	if !strings.EqualFold(r.Method, "POST") {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Only POST method currently allowed"))
-		return
+func (i *incomingWebhook) writeString(w http.ResponseWriter, str string) {
+	if _, err := w.Write([]byte(str)); err != nil {
+		i.handleRequestsLogger.WithError(err).Error("Unable to write: " + str)
 	}
+}
 
+func (i *incomingWebhook) writeTokenInputForm(w http.ResponseWriter) {
+	i.writeString(w, fmt.Sprintf(`
+		<html>
+			<head>
+				<style>
+					input { 
+						width: 450px;
+					}
+				</style>
+			</head>
+			<body>
+				<form method="POST">
+					Token:
+					<br>
+					<input readonly type="text" value="%s">
+					
+					<hr>
+					<input type="text" name="token">
+					<button type="submit">submit</button>
+				</form>
+			</body>
+		</html>
+	`, i.randomToken))
+}
+
+func (i *incomingWebhook) onDefaultGETRequest(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	i.writeTokenInputForm(w)
+}
+
+func (i *incomingWebhook) onDefaultPOSTRequest(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		i.handlePostRequestLogger.WithError(err).WithFields(map[string]interface{}{
+		i.handleRequestsLogger.WithError(err).WithFields(map[string]interface{}{
 			"remote-address":  r.RemoteAddr,
 			"request-headers": r.Header,
 		}).Warn(fmt.Sprintf("Failed to read POST body"))
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(fmt.Sprintf("Body is empty, try again.")))
+		i.writeString(w, fmt.Sprintf("Body is empty, try again."))
 		return
 	}
 
 	bodyStr := strings.TrimSpace(string(body))
+
+	if strings.HasPrefix(bodyStr, "token=") {
+		bodyStr = bodyStr[len("token="):]
+	}
+
 	if bodyStr == i.randomToken {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Thank you, token is accepted"))
+		i.writeString(w, fmt.Sprintf(`Thank you, token is accepted.`))
 		i.resultChan <- &incomingWebhookResult{}
 		return
 	}
 
 	if bodyStr == i.abortKeyword {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Abort keyword received"))
+		i.writeString(w, fmt.Sprintf("Abort keyword received."))
 		i.resultChan <- &incomingWebhookResult{
 			Error: errors.New("User aborted with keyword " + i.abortKeyword),
 		}
@@ -71,7 +106,19 @@ func (i *incomingWebhook) handlePostRequest(w http.ResponseWriter, r *http.Reque
 	}
 
 	w.WriteHeader(http.StatusBadRequest)
-	w.Write([]byte(fmt.Sprintf("Unexpected input '%s' received, try again.", bodyStr)))
+	i.writeString(w, fmt.Sprintf("Unexpected input '%s' received, try again.", bodyStr))
+}
+
+func (i *incomingWebhook) handleDefaultRequest(w http.ResponseWriter, r *http.Request) {
+	switch strings.ToUpper(r.Method) {
+	case "GET":
+		i.onDefaultGETRequest(w, r)
+	case "POST":
+		i.onDefaultPOSTRequest(w, r)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		i.writeString(w, "Only POST method currently allowed")
+	}
 }
 
 func (i *incomingWebhook) runServer(logger logging.Logger, server *http.Server) {
@@ -84,7 +131,7 @@ func (i *incomingWebhook) runServer(logger logging.Logger, server *http.Server) 
 				ServerClosed: true,
 			}
 		}
-		logger.Debug("HTTP server connection closed")
+		// logger.Debug("HTTP server connection closed")
 		return
 	}
 }
@@ -94,8 +141,8 @@ func (i *incomingWebhook) runAndWait(logger logging.Logger) error {
 	i.resultChan = resultChan
 
 	mux := http.NewServeMux()
-	i.handlePostRequestLogger = logger
-	mux.HandleFunc("/", i.handlePostRequest)
+	i.handleRequestsLogger = logger
+	mux.HandleFunc("/", i.handleDefaultRequest)
 
 	server := &http.Server{
 		Addr:    i.listenHTTPAddress,
